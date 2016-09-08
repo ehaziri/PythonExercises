e7e1cdcf17becc5f99604bfdc786507732905eef
xonaxona <egzonahaziri@live.com>
2016-09-08 19:47:48 +0200

    Added grades.py and names.py for the 1st week

---
+++ grades.py
@@ -0,0 +1,15 @@
+print ("Scores and Grades")
+
+for i in range(0,10):
+    piket = input("Shkruaj piket: ")
+
+    if(piket>=60 and piket<70):
+        print("Score: ", piket,"; Your grade is D.")
+    elif(piket>=70 and piket<80):
+        print("Score: " ,piket ,"; Your grade is C.")
+    elif(piket>=80 and piket<90):
+        print("Score: ", piket ,"; Your grade is B.")
+    elif(piket>=90 and piket<=100):
+        print("Score: ", piket ,"; Your grade is A.")
+    else:
+        print("Piket e shenuara duhet te jene ne intervalin [60,100]")
---
+++ names.py
@@ -0,0 +1,39 @@
+#1
+# students = [
+#     {'first_name' : 'Michael', 'last_name' : 'Jordan'},
+#     {'first_name' : 'John', 'last_name' : 'Rosales'},
+#     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+# ]
+#
+# for dictionary in students:
+#         print dictionary["first_name"], dictionary["last_name"]
+
+
+#2
+users = {
+    'Students' : [
+        {'first_name' : "Michael", "last_name" : 'Jordan'},
+        {'first_name' : "John", "last_name" : 'Rosales'},
+        {'first_name' : "Mark", "last_name" : 'Guillen'},
+        {'first_name' : "KB", "last_name" : 'Tonel'}
+    ],
+    'Instructors' : [
+        {'first_name' : "Michael", "last_name" : 'Choi'},
+        {'first_name' : "Martin", "last_name" : 'Puryear'}
+    ]
+}
+
+
+
+for key, data in users.items():
+    print key
+    i = 1
+    for value in data:
+            fn = value["first_name"]
+            fn_len = len(fn)
+            ln = value["last_name"]
+            ln_len = len(ln)
+            full_len = fn_len + ln_len
+            print  i, "-", fn.upper() , ln.upper() , "-", full_len
+            i += 1
